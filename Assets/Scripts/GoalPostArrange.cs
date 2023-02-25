cf4a7910079699ba2e6ada728b18af63ee85d27e
Ko Minhyeok <fnels154@gmail.com>
2023-02-25 16:21:07 +0900

    Solved Error

--- Assets/Scripts/GoalPostArrange.cs
+++ Assets/Scripts/GoalPostArrange.cs
@@ -1,5 +1,9 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR;
+using UnityEngine.UIElements;
+using UnityEngine.XR.ARFoundation;
 
 // 골포스트를 배치하고 싶다
 
@@ -11,18 +15,67 @@ using UnityEngine.XR;
 
 public class GoalPostArrange : MonoBehaviour
 {
+    ARRaycastManager arRaycastManager;
+
     public GameObject goalPostFactory;
 
+    public GameObject arCamera;
+    public GameObject unityCamera;
+    public GameObject unityWall;
+
     bool isCreated = false;
+
+    Vector3 center;
+
+    void Start()
+    {
+        center = new Vector2(Screen.width * 0.5f, Screen.height * 0.5f);
+
+        arRaycastManager = GetComponent<ARRaycastManager>();
+
+#if UNITY_EDITOR
+        arCamera.SetActive(false);
+        unityCamera.SetActive(true);
+        unityWall.SetActive(true);
+#elif UNITY_ANDROID
+        arCamera.SetActive(true);
+        unityCamera.SetActive(false);
+        unityWall.SetActive(false);
+#endif
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.touchCount > 0 && !isCreated)
+#if UNITY_EDITOR
+        UpdateForUnityEditor();        
+
+#elif UNITY_ANDROID
+        UpdateForAndroid();
+
+#endif
+        if (isCreated)
+        {
+            unityWall.SetActive(false);
+        }
+    }
+
+    private void UpdateForAndroid()
+    {
+        List<ARRaycastHit> arHit = new List<ARRaycastHit>();
+     
+        if (arRaycastManager.Raycast(center, arHit))
+        {
+            unityWall.SetActive(true);
+            unityWall.transform.position = arHit[0].pose.position;
+            unityWall.transform.rotation = arHit[0].pose.rotation;
+        }
+
+        if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
 
-            if (touch.phase == TouchPhase.Began)
+            if (touch.phase == TouchPhase.Began && !isCreated)
             {
                 Ray ray = Camera.main.ScreenPointToRay(touch.position);
 
@@ -36,8 +89,33 @@ public class GoalPostArrange : MonoBehaviour
 
                     goalPost.transform.position = hitInfo.point;
 
-                    goalPost.transform.forward = -Camera.main.transform.forward;
+                    goalPost.transform.forward = hitInfo.normal;
+
+                    goalPost.transform.Rotate(90.0f, 0.0f, 0.0f);
                 }
+            }
+        }
+    }
+
+    private void UpdateForUnityEditor()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        RaycastHit hitInfo;
+
+        if (Input.GetButtonDown("Fire1") && !isCreated)
+        {
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                isCreated = true;
+
+                GameObject goalPost = GameObject.Instantiate(goalPostFactory);
+
+                goalPost.transform.position = hitInfo.point;
+
+                goalPost.transform.forward = hitInfo.normal;
+
+                goalPost.transform.Rotate(90.0f, 0.0f, 0.0f);
             }
         }
     }
